1b2d538e0a7b04ccb2c878277375a67c14a79289
AdrienLapasset <aflapasset@gmail.com>
2024-02-01 14:17:46 +0100

    detect page is loaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,23 @@ export default function Home({ illustrations, draftMode, events }) {
   const [illustrationIndex, setIllustrationIndex] = useState();
 
   useEffect(() => {
+    // callback function to call when event triggers
+    const onPageLoad = () => {
+      console.log("page loaded");
+      // do something else
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad, false);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
+  useEffect(() => {
     const findIllustration = (illustration) =>
       illustration.slug.current === router.query.illustrationSlug;
     const illustration = illustrations.find(findIllustration);
