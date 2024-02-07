6ac19f4c45e8fe35d410e6b851dd33a478b67509
AdrienLapasset <aflapasset@gmail.com>
2024-02-07 18:49:05 +0100

    selected format iage

--- src/components/IllustrationModal.js
+++ src/components/IllustrationModal.js
@@ -227,7 +227,7 @@ export default function IllustrationModal({ illustrations }) {
 
   const mainImageProps = useNextSanityImage(
     sanityClient,
-    illustration?.mainImage
+    selectedFormat?.image
   );
   const titleImageProps = useNextSanityImage(
     sanityClient,
